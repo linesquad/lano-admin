897f2f223b4b3e541dc189e011bc7b20b4c7c2dd
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-10-19 19:01:00 +0400

    implement home page design

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,5 +1,13 @@
+import welcomeBird from "/images/welcomeBird.svg";
+import welcome from "/images/Welcome.svg";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center mt-[152px] ">
+      <img src={welcomeBird} alt="welcome-bird" className="ml-[145px]" />
+      <img src={welcome} alt="welcome" />
+    </div>
+  );
 };
 
 export default Home;
