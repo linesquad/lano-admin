a3e4c835b520b2b288dd84562148b73943358920
marriamdd <davitashvili.m3@gmail.com>
2024-10-19 23:00:09 +0400

    wip_fix:form positioning

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,7 @@ import { login } from "../services/auth";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
+
 interface IFormInput {
   name: string;
   password: string;
@@ -16,71 +17,65 @@ export default function Login() {
   } = useForm<IFormInput>();
   const navigate = useNavigate();
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    console.log(data);
-
     const success = login(data.name, data.password);
     if (success) {
       toast.success("Login successful!");
       navigate("/");
-      console.log("Login successful!");
     } else {
       toast.error("Invalid credentials");
-      console.log("Invalid credentials");
     }
   };
 
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        backgroundImage: "url(/loginGB.svg)",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "right top",
-        backgroundSize: "contain",
-        backgroundAttachment: "fixed",
-      }}
-      className="bg-white relative items-center justify-center flex w-full h-[100vh]"
-    >
-      <ToastContainer />
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex items-center gap-[16px] justify-center pb-[40px]">
-            <img src="/logo.svg" alt="" />
-            <span className="text-[#EE5335] text-[24px] font-[400] ">LANO</span>
-          </div>
-
-          <div className="flex flex-col  pl-[50px] gap-[16px]">
-            <input
-              className=" h-[48px] py-[8px] text-[14px] w-[342px] px-[16px] rounded-[7px] border border-[#00000066] bg-[#FFF]"
-              placeholder="სახელი"
-              {...register("name", { required: "სახელი სავალდებულოა" })}
-            />
-
-            <input
-              placeholder="პაროლი"
-              type="password"
-              className=" h-[48px] py-[8px] text-[14px] w-[342px] px-[16px] rounded-[7px] border border-[#00000066] bg-[#FFF]"
-              {...register("password", { required: "პაროლი სავალდებულოა" })}
-            />
-            <div className="h-[5px]">
-              {(errors.name || errors.password) && (
-                <span className="text-[#EE5335]  text-[13px] ">
-                  შეავსეთ ორივე ველი
+    <div className="w-full flex justify-end">
+      <div className="bg-white relative flex items-center justify-end w-[1400px] ">
+        <ToastContainer />
+        <div className="w-[324px] ">
+          <form
+            className="flex flex-col absolute  right-[439px] bottom-[160px]  "
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="pl-[40px] mb-[20px]">
+              <div className="flex items-center gap-[16px] justify-center pb-[40px]">
+                <img src="/logo.svg" alt="" />
+                <span className="text-[#EE5335] text-[24px] font-[400]">
+                  LANO
                 </span>
-              )}
-            </div>
-          </div>
+              </div>
 
-          <div>
-            <button
-              className="bg-[#EE5335] text-[#FFFFFF] absolute bottom-[170px] right-[560.5px] w-[342px] h-[48px] rounded-[7px] py-[8px] px-[10px] text-[14px] font-[600] "
-              type="submit"
-            >
-              შესვლა
-            </button>
-          </div>
-        </form>
+              <div className="flex flex-col  gap-[16px]">
+                <input
+                  className="h-[48px] py-[8px] text-[14px] w-[342px] px-[16px] rounded-[7px] border border-[#00000066] bg-[#FFF]"
+                  placeholder="სახელი"
+                  {...register("name", { required: "სახელი სავალდებულოა" })}
+                />
+
+                <input
+                  placeholder="პაროლი"
+                  type="password"
+                  className="h-[48px] py-[8px]  text-[14px] w-[342px] px-[16px] rounded-[7px] border border-[#00000066] bg-[#FFF]"
+                  {...register("password", { required: "პაროლი სავალდებულოა" })}
+                />
+                <div className="h-[5px]">
+                  {(errors.name || errors.password) && (
+                    <span className="text-[#EE5335]  text-[13px]">
+                      შეავსეთ ორივე ველი
+                    </span>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div>
+              <button
+                className="bg-[#EE5335]  text-[#FFFFFF] w-[342px] h-[48px] rounded-[7px] py-[8px] px-[10px] text-[14px] font-[600] "
+                type="submit"
+              >
+                შესვლა
+              </button>
+            </div>
+          </form>
+        </div>
+        <img className="h-[700px]" src="/loginGB.svg" alt="" />
       </div>
     </div>
   );
