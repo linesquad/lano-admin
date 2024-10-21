0d135a31f1e63519e7cce164d497c0c15e5cc72f
marriamdd <davitashvili.m3@gmail.com>
2024-10-21 19:54:02 +0400

    fix:input clearing

--- src/components/productPage/Search.tsx
+++ src/components/productPage/Search.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSearchProducts } from "../../hook/useSearching";
 import SearchedProductsModal from "./SearchedProductsModal";
 
@@ -11,7 +11,7 @@ const Search = () => {
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedInput(input.length > 0 ? input : "");
-    }, 1000);
+    }, 500);
 
     return () => {
       clearTimeout(handler);
@@ -19,19 +19,22 @@ const Search = () => {
   }, [input]);
 
   useEffect(() => {
-    if (data && Array.isArray(data) && data.length <= 1) {
+    if (data && Array.isArray(data) && data.length >= 1) {
       setShowSearchedProducts(true);
+      console.log("here");
     } else {
       setShowSearchedProducts(false);
     }
   }, [data]);
   console.log(data, " search data");
 
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <div className="relative">
       <div className="flex items-center justify-center rounded-lg gap-1 py-2 pl-[10px] border-solid border-[1px] border-[rgba(0, 0, 0, 0.2)] rounded-[7px] cursor-pointer">
         <img src="/search.svg" alt="search" className="w-6 h-6" />
         <input
+          ref={inputRef}
           onChange={(e) => setInput(e.target.value)}
           value={input}
           type="text"
@@ -41,9 +44,11 @@ const Search = () => {
       </div>
       {showSearchedProducts && (
         <SearchedProductsModal
+          showSearchedProducts={showSearchedProducts}
           onClose={() => setShowSearchedProducts(false)}
           clearInput={() => setInput("")}
           data={data}
+          inputRef={inputRef.current}
         />
       )}
       <div className="h-[10px]">
--- src/components/productPage/SearchedProductsModal.tsx
+++ src/components/productPage/SearchedProductsModal.tsx
@@ -7,11 +7,15 @@ interface SearchedProductsModalProps {
   data: SearchResponse | undefined;
   onClose: () => void;
   clearInput: () => void;
+  showSearchedProducts: boolean;
+  inputRef: HTMLInputElement | null;
 }
 export default function SearchedProductsModal({
   data,
   onClose,
   clearInput,
+  showSearchedProducts,
+  inputRef,
 }: SearchedProductsModalProps) {
   console.log(data, "from modal");
   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
@@ -26,7 +30,12 @@ export default function SearchedProductsModal({
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(e.target as Node) &&
+        inputRef &&
+        !inputRef.contains(e.target as Node)
+      ) {
         onClose();
         clearInput();
       }
@@ -38,47 +47,53 @@ export default function SearchedProductsModal({
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [onClose, clearInput]);
+  console.log(showSearchedProducts, "showSearchedProducts");
   return (
-    <div
-      ref={modalRef}
-      className="absolute top-[70px] w-[400px] max-h-[300px]  overflow-y-auto left-[-21px] shadow-lg shadow-gray-500 rounded-[7px] p-[20px] flex flex-col bg-[white]"
-    >
-      {data &&
-        Array.isArray(data) &&
-        data.length > 0 &&
-        data.map((product) => (
-          <ul
-            key={product._id}
-            className="text-black font-normal flex items-center gap-[30px] text-sm py-[21px]"
-          >
-            <img
-              src={product.image}
-              alt="product"
-              className="w-[30px] h-[30px]"
-            />
-            <span className="text-nowrap">{product.title}</span>
-            <div className="flex gap-[10px]">
-              <li className="text-[12px] line-through text-nowrap">{`${product.price.$numberDecimal} ₾`}</li>
-              <li className="text-[#EE5335] text-[12px] text-nowrap font-semibold">{`${product.discount} ₾`}</li>
-            </div>
-            <li className="flex gap-[5px] items-center">
-              <button className="p-2 cursor-pointer ">
-                <img src="/edit.svg" alt="edit" />
-              </button>
-              <button
-                onClick={() => setDeleteModalOpen(true)}
-                className="p-2 cursor-pointer "
-              >
-                <img src="/delete.svg" alt="delete" />
-              </button>
-            </li>
-            <DeleteModal
-              isOpen={isDeleteModalOpen}
-              onClose={() => setDeleteModalOpen(false)}
-              onConfirm={() => handleDeleteProduct(product._id)}
-            />
-          </ul>
-        ))}
+    <div>
+      <div
+        ref={modalRef}
+        className={`${
+          showSearchedProducts &&
+          "absolute top-[70px] w-[400px] max-h-[300px]  overflow-y-auto left-[-21px] shadow-lg shadow-gray-500 rounded-[7px] p-[20px] flex flex-col bg-[white]"
+        }`}
+      >
+        {data &&
+          Array.isArray(data) &&
+          data.length > 0 &&
+          data.map((product) => (
+            <ul
+              key={product._id}
+              className="text-black font-normal flex items-center gap-[30px] text-sm py-[21px]"
+            >
+              <img
+                src={product.image}
+                alt="product"
+                className="w-[30px] h-[30px]"
+              />
+              <span className="text-nowrap">{product.title}</span>
+              <div className="flex gap-[10px]">
+                <li className="text-[12px] line-through text-nowrap">{`${product.price.$numberDecimal} ₾`}</li>
+                <li className="text-[#EE5335] text-[12px] text-nowrap font-semibold">{`${product.discount} ₾`}</li>
+              </div>
+              <li className="flex gap-[5px] items-center">
+                <button className="p-2 cursor-pointer ">
+                  <img src="/edit.svg" alt="edit" />
+                </button>
+                <button
+                  onClick={() => setDeleteModalOpen(true)}
+                  className="p-2 cursor-pointer "
+                >
+                  <img src="/delete.svg" alt="delete" />
+                </button>
+              </li>
+              <DeleteModal
+                isOpen={isDeleteModalOpen}
+                onClose={() => setDeleteModalOpen(false)}
+                onConfirm={() => handleDeleteProduct(product._id)}
+              />
+            </ul>
+          ))}
+      </div>
     </div>
   );
 }
