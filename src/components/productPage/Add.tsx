a984dd5dbf35abc6f4ef71d822da466c56302cbc
Nugo Bibilashvili <bibilashvili6@gmail.com>
2024-10-22 13:46:47 -0700

    fix add button and delete old images

--- src/components/productPage/Add.tsx
+++ src/components/productPage/Add.tsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 const Add = () => {
   return (
-    <div className="flex justify-center items-center bg-[#EE5335] px-[50px] py-[10.5px] rounded-[7px] cursor-pointer">
-      <Link to="/products/add">
-        <span className="text-white text-sm font-semibold ">დამატება</span>
-      </Link>
-    </div>
+    <Link
+      to="/products/add"
+      className="flex justify-center items-center bg-[#EE5335] px-[50px] py-[10.5px] rounded-[7px] cursor-pointer"
+    >
+      <span className="text-white text-sm font-semibold ">დამატება</span>
+    </Link>
   );
 };
 
